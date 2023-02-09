6e6555e9ea06ab4f309b138dd28f9c9ef67c6837
George Hastings <19ghastings@bromsgrove-school.co.uk>
2023-02-09 14:36:19 +0000

    Final modifications

--- main.py
+++ main.py
@@ -8,21 +8,21 @@
 def take_input(str):
     try:
         dimension = float(input(f"Enter {str}: "))
-        return dimension
-    except TypeError:
+    except ValueError:
         print("You must enter a number.")
-        take_input(str)
+        dimension = take_input(str)
+    return dimension
     # I considered validating the dimensions of the room but decided that
-    # Not doing so may provide more utility from a general perspective as 
-    # The program could be used on rooms in models or towers with abnormal 
-    # dimensions. 
+    # not doing so may provide more utility from a general perspective as 
+    # the program could be used on rooms in models or towers with abnormal 
+    # dimensions 
     # This simple method of validation simply attempts to convert the user's
     # input into a float. If a TypeError is thrown it is assumed their input 
-    # is not in the correct form.
+    # is not in the correct form
 length = take_input("length")
 width = take_input("width")
 height = take_input("height")
-# The dimensions are likely not regular, so we convert inputs to floats to be safe.
+# The dimensions are likely not regular, so we convert inputs to floats to be safe
 # We are assuming that the room is of regular proportions
 # We are also ignoring design features which might impact the proportions e.g. coving
     
@@ -30,18 +30,22 @@ height = take_input("height")
 def floor_area(length, width):
     return (length * width)
 
+
+# Function for finding the surface area of the room
 def surface_area(length, width, height):
     pair1 = 2 * (length * width) # Floor and ceiling S.A.
     pair2 = 2 * (length * height) # Pair of walls
     pair3 = 2 * (width * height) # Other pair of walls
     return (pair1 + pair2 + pair3) # Return the sum of all 6 walls
 
+# Function for finding the volume of the room
 def volume(length, width, height):
     return (length * width * height)
 
-print(floor_area(length, width))
-print(surface_area(length, width, height))
-print(volume(length, width, height))
+# Print the results of the calculations by calling the functions.
+print("Area of the floor is", floor_area(length, width), "metres squared")
+print("Surface area of the room is", surface_area(length, width, height), "metres squared")
+print("Volume of the room is", volume(length, width, height), "metres cubed")
 
 
 
